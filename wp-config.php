6a54563aaa93e38e5f628748d504811b816176f2
Etienne Reboul <etienne.reboul@gmail.com>
2018-02-15 20:18:38 +0100

    reinit

--- wp-config.php
+++ wp-config.php
@@ -5,16 +5,16 @@ define('FS_METHOD', 'direct');
 
 // ** MySQL settings ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'db618542866');
+define('DB_NAME', 'db623366559');
 
 /** MySQL database username */
-define('DB_USER', 'dbo618542866');
+define('DB_USER', 'dbo623366559');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'zNmMwvjOrbkSwwDioNmA');
+define('DB_PASSWORD', 'HAzzqwAKndzRKPXYzWqr');
 
 /** MySQL hostname */
-define('DB_HOST', 'db618542866.db.1and1.com');
+define('DB_HOST', 'db623366559.db.1and1.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -22,17 +22,17 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('AUTH_KEY',         '>OPC`4?%tC#-7q|^eT%6;~${||G7^[Y(.LbWmFH3/!*qW{R9N4V<76+J7.zj{*q ');
-define('SECURE_AUTH_KEY',  '{}m&Xp4n(q f:d-M(Y)!-1GYTv_];>rgn|ez~)@R&y+P)CilZ}%)uOWO+rf~+-*d');
-define('LOGGED_IN_KEY',    'FO{zRP5oIn33x/=g~Q}J.~>^-[~Qp-y#*IeoS|S[ls50P1?^{O@KeHB9ODk`cfhT');
-define('NONCE_KEY',        'dh5n*8`jqW>v}rJ(|HQB=+y87!j[Jz0$[wUReJOzQie.o@rkf,f!_,~?6v=>BUc#');
-define('AUTH_SALT',        'XT0aB|S+#&~}VHgWc-)<qU:8opZ]az^L2r(=h+M*D;vJ!FQgdy:}<b]!|Jc6 GBL');
-define('SECURE_AUTH_SALT', 'n<Fdf8FNuifAjP 0d0d:NjHjx?z4h`PaGK![Y!D)5S<N2S+r(sW|.l;^<=Y=,w7>');
-define('LOGGED_IN_SALT',   'CgDb(g(6.YCkeuLx~:r%_#W;IF+Q5a[aQ4n`z+TrB,uk+9_qQEl~d;WIN`B0g&<h');
-define('NONCE_SALT',       '=h2UWtneQ2QZ@,w`joeq)|E[$ Vukd7ljqA`wNy%<YU]d `!r^8:jOkGN`t/@|*a');
+define('AUTH_KEY',         'j{A oAF[V/goznnwHuCi#n(eK9P[<I1bgeG-*~Kr{vI7_tu3H5!iLLOAM1@w2upq');
+define('SECURE_AUTH_KEY',  '41(>)A^hQW}*|`U JN1C$>=zF<.R2v96^|*ce>l%0.L!ubr[C?smAb6V_B-w%+h*');
+define('LOGGED_IN_KEY',    '-{f1H!%Vk@F *0vLDJudHus]43kTYu)<0ykXr_-FY^8C_,t;f65*[5|`R+@`?o+V');
+define('NONCE_KEY',        'de8V|zkk(xV#kZ3t~`>ND$RnVPh<y@fg.?D*9pQRKMDM4OBRl<|#nZ-+xRz8`p]t');
+define('AUTH_SALT',        'YS9A$mLxm?<bz[nMy9+v_b/b.68fR#{<sGh}-%+]HLd2EN8 ID-+n#?Jt+77{vLE');
+define('SECURE_AUTH_SALT', 'eg%Jd7YMC;yc>i~5{B&qTgS^J~5ZAw1$!P;wO2j0TF[!ch|Sl.Ifz|1MiE@T~lFK');
+define('LOGGED_IN_SALT',   'p1c43HW?vJC;9U- Pf?j8 m4eGIf&&AHSKj%l7Q-+O-x|{nO?8,Lw<117H6=.Hzb');
+define('NONCE_SALT',       'TYNfL*[u[4K]nD1>2okgJW>@m@YCB~Bb0-dzVH#)sZM|j5tYLot6*jPp;Z$s+B(N');
 
 
-$table_prefix = 'FryhcvjE';
+$table_prefix = 'hrIBBERi';
 
 
 
---
+++ wp-content/themes/blogsixteen/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BlogSixteen
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<div class="footer-content">
+			<?php wp_footer(); ?>
+		</div>
+
+	</footer><!-- #colophon -->
+
+	</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
